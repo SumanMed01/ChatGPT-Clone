0ad325eed7d219b163693d4b6328a0f0f8f1a307
Kashyap Das <kashyap.education2234@gmail.com>
2024-09-21 18:20:54 +0530

    Adding logic in the ChatGPT Auto button

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Collaboration added successfully");
+// onClick display toogle in ChatGPT Auto button
+let btn = document.querySelector("#dropdown")
+btn.addEventListener('click',()=>{
+    document.querySelector(".dropdown-page").classList.toggle("nonView");
+})
